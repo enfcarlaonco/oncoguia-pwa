bbcf2c3c8569d9b244f2519b15037f8bb326e936
enfcarlaonco <enfcarlaonco@gmail.com>
2026-04-20 19:54:56 -0300

    Disable cache SW

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,7 @@
-const CACHE = 'oncoguia-v4';
-const ASSETS = ['/', '/index.html', '/css/styles.css', '/js/app.js'];
-
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-    self.skipWaiting();
-});
-
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
     e.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-        )
+        caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
     );
     self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-    if (e.request.url.includes('/api/')) return;
-    e.respondWith(
-        fetch(e.request).catch(() => caches.match(e.request))
-    );
 });
